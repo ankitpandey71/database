15ba5594446be29cab1ca558e0a3ecac8f7f154b
moraj <dhwajmoraj7@gmail.com>
2023-03-29 16:13:59 +0530

    Create a database

--- app/models/index.js
+++ app/models/index.js
@@ -1,6 +1,6 @@
 const dbConfig = require("../config/db.config.js");
 
-const Sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
   "postgres://res_pos_sql_2f6r_user:vfUbLySlMcEF5vPeR69JYYtaCwLq3ohD@dpg-cge1rco2qv2bbhmdgvc0-a.singapore-postgres.render.com/res_pos_sql_2f6r?ssl=true"
@@ -14,3 +14,32 @@ sequelize
   .catch((err) => {
     console.log("Unable to connect to the database:", err);
   });
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
+
+const user = new User({ id: 1, firstName: "Moraj", lastName: "Dhwaj" });
+console.log(user);
